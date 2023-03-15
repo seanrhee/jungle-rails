46a1a99f69fb922e6563a4ec5ca3ee2ea108dda9
Sean Lee <lee.sean.cw@gmail.com>
2023-03-15 19:17:23 -0400

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,10 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
-
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
